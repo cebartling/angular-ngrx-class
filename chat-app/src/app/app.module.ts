391b29f13d0dea9b6e231fc4199b8a188189a2d7
Christopher Bartling <chris.bartling@gmail.com>
2017-03-25 23:53:24 -0500

    Scaffolding out components.

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSelectionComponent } from './thread-selection/thread-selection.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSelectionComponent,
+    MessageSectionComponent,
+    ThreadSectionComponent,
+    ThreadListComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule,
